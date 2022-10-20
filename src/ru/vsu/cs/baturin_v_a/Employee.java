9800625e2389ac1373ed7614ef2ca384215e0cf0
CooIDragon <90614965+CooIDragon@users.noreply.github.com>
2022-10-20 03:41:06 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/baturin_v_a/Employee.java
@@ -0,0 +1,22 @@
+package ru.vsu.cs.baturin_v_a;
+
+public class Employee {
+    public String surname;
+    public String name;
+    public String email;
+    public String phoneNumber;
+    public String city;
+    public String salary;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+}
